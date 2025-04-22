eb4e164a7f65f11963549b43e22698a338ed0e6d
Tolga <cavga228@gmail.com>
2025-04-22 03:35:56 +0300

    updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { HiMenu, HiX } from 'react-icons/hi';
-import { FaCode } from 'react-icons/fa';
+import React, { useState } from "react";
+import { HiMenu, HiX } from "react-icons/hi";
+import { FaCode } from "react-icons/fa";
 
 type NavLink = {
   id: string;
@@ -24,7 +24,10 @@ const Navbar: React.FC = () => {
     <nav className="bg-white text-white dark:bg-gray-900 sticky top-0 z-50 shadow-md border-b border-gray-200 dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         {/* Logo */}
-        <a href="#hero" className="flex items-center text-2xl font-bold dark:text-white">
+        <a
+          href="#hero"
+          className="flex items-center text-2xl font-bold dark:text-white"
+        >
           Tolga Çavga
           <FaCode className="text-3xl ml-2 align-middle dark:text-white" />
         </a>
@@ -47,13 +50,18 @@ const Navbar: React.FC = () => {
         </div>
 
         {/* Menü Linkleri Burada */}
-        <div className={`${isMenuOpen ? "block" : "hidden"} w-full md:block md:w-auto`} id="navbar-menu">
+        <div
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } w-full md:block md:w-auto`}
+          id="navbar-menu"
+        >
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             {navLinks.map((link) => (
               <li key={link.id}>
                 <a
                   href={`#${link.id}`}
-                  className="block py-2 pl-3 pr-4 text-lg rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 pl-3 pr-4 text-lg rounded text-gray-800 dark:text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:hover:bg-gray-700 dark:hover:text-blue-400 md:dark:hover:bg-transparent"
                   onClick={() => setIsMenuOpen(false)}
                 >
                   {link.label}
